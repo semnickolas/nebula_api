620419300b16114b937f1055012ca81d6c321c69
escobar@escobar <escobar.netpeak@gmail.com>
2020-04-27 14:49:46 +0300

    update run.sh

--- bin/run.sh
+++ bin/run.sh
@@ -2,7 +2,7 @@
 
 docker-compose up -d --build
 
-docker exec -it nebula_api_php php bin/console doctrine:migrations:migrate --no-interaction
-docker exec -it nebula_api_php php bin/console doctrine:fixtures:load --no-interaction
+docker-compose exec php php bin/console doctrine:migrations:migrate --no-interaction
+docker-compose exec php php bin/console doctrine:fixtures:load --no-interaction
 
-docker exec -it nebula_api_php  php bin/console messenger:consume payments
+docker-compose exec php php bin/console messenger:consume payments
